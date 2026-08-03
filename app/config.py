8b30b757a88c3cd09c4c52161e8fc49a27c7164b
Le Trieu Dai <dai.220046@tbd.edu.vn>
2026-08-03 15:22:28 +0700

    Add default config values

--- app/config.py
+++ app/config.py
@@ -3,6 +3,17 @@ import os
 
 load_dotenv()
 
-MONGO_URI = os.getenv("MONGO_URI")
-DATABASE_NAME = os.getenv("DATABASE_NAME")
-COLLECTION_NAME = os.getenv("COLLECTION_NAME")
+MONGO_URI = os.getenv(
+    "MONGO_URI",
+    "mongodb://localhost:27017"
+)
+
+DATABASE_NAME = os.getenv(
+    "DATABASE_NAME",
+    "todo_db"
+)
+
+COLLECTION_NAME = os.getenv(
+    "COLLECTION_NAME",
+    "tasks"
+)
